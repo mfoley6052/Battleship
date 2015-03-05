3f505df28a5711c4b2240cedcb763bf4d349ae7f
Mitchell Foley <mitch.foley@mail.com>
2015-03-05 10:57:09 -0500

    Initial Commit
    Start of structure chart, first function complete

---
+++ Battleship.py
@@ -0,0 +1,20 @@
+#Mitchell Foley
+#1316506
+#foleymb
+#
+#BattleShip
+#
+
+#def main():
+#    printIntro()
+#    setup()
+#    game()
+#    printResults()
+
+def printIntro():
+    print("This is a game of BattleShip, you versus the computer. ")
+    print("You will select the placements of a Destroyer of size 2,")
+    print(" a Cruiser of size 3, and an Aircraft Carrier of size 5. ")
+    print("The game board is 7x7 each.")
+    print("The computer will start first after ships have been placed.")
+    
