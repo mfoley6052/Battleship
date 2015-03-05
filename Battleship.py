3bb3e70e5f35950ef9cbb5c2b16c555e059a97d1
Mitchell Foley <mitch.foley@mail.com>
2015-03-05 11:20:56 -0500

    Added player class

--- Battleship.py
+++ Battleship.py
@@ -5,6 +5,11 @@
 #BattleShip
 #
 
+class player():
+   def __init__(self):
+       self.myFleet = [[None]*7]*7
+       self.myHits = [[None]*7]*7
+
 #def main():
 #    printIntro()
 #    setup()
@@ -14,7 +19,18 @@
 def printIntro():
     print("This is a game of BattleShip, you versus the computer. ")
     print("You will select the placements of a Destroyer of size 2,")
-    print(" a Cruiser of size 3, and an Aircraft Carrier of size 5. ")
+    print(" a Cruiser of size 3, and an Aircraft Carrier of size 5.")
     print("The game board is 7x7 each.")
     print("The computer will start first after ships have been placed.")
-    
+
+
+#def setup():
+#    player = [None,None]
+#    
+#    for i in range(2):
+#         player[i] = createPlayer(i)
+#def createPlayer(pl):
+#    myFleet = [[None*7]*7]
+#    myHits = [[None*7]*7]
+#    myFleet[0],myFleet[1] = placeShips()
+#        
